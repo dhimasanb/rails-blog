451d26e1f7fb5e155165b6f4a09fde2b8b0c29ef
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-25 01:04:11 +0700

    tambah create comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+
+  def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+		redirect_to post_path(@post)
+	end
 end
